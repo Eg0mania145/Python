5d2b2b2d0c8bead5eb5f6ba124a528fb1be93aa5
25wigr <148889447+Eg0mania145@users.noreply.github.com>
2025-11-13 09:55:26 +0000

    solved

--- Om jag skulle gör något.py/Om jag måste arbeta 2.py
+++ Om jag skulle gör något.py/Om jag måste arbeta 2.py
@@ -2,27 +2,38 @@ import os
 import time
 
 os.system('cls')
+text = []
+nytext = " "
+bokstav = []
+play = True
+again = None
+
+while play == True:
+    text = input("Skriv in en textsträng : ").strip()
+    bokstav = input("Vilken bokstav vill du ta bort? : ").strip()
+
+    time.sleep(1)
+    os.system('cls')
 
 
-text = input("Skriv in en textsträng : ").strip()
-bokstav = input("Vilken bokstav vill du ta bort? : ").strip()
-
-time.sleep(1)
-os.system('cls')
+    if len(bokstav) != 1:
+        print("Du måste skriva exakt EN bokstav!")
+        exit(0)
 
 
-if len(bokstav) != 1:
-    print("Du måste skriva exakt EN bokstav!")
-    exit(0)
+    nytext = " "
+    for char in text:
+        if char != bokstav:
+            nytext += char
 
 
+    print("\nResultatet blir : ")
+    print(nytext)
+    if input("vill du köra igen : ").lower() == ("n") :
+        break
+    else:
+        pass
 
-nytext = text.replace(bokstav, "").replace(bokstav.upper(), "")
 
-
-print("\nResultatet blir : ")
-print(nytext)
-
-
-time.sleep(1)
-os.system('cls')
+    time.sleep(1)
+    os.system('cls')
