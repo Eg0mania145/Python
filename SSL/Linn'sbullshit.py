f8a4b4c03c38cb96a3bd6786b69e6b2995d1a19f
25wigr <148889447+Eg0mania145@users.noreply.github.com>
2025-11-20 09:27:12 +0000

    ny skit

---
+++ SSL/Linn'sbullshit.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+os.system('cls')
+
+tal = ()
+
+x = input("Skriv in antalet tal : ")
+for i in range(1,x):
+    tal = input("skriv in dina tal")
+
+print(tal)
+time.sleep(2)
+os.system('cls')
