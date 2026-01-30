cffbeeca2a935bb7449bc931ed13bab74c100b80
25wigr <148889447+Eg0mania145@users.noreply.github.com>
2026-01-30 10:17:33 +0000

    fix

--- SSL/Avstånd mellan lins hjärnceller är för stort.py
+++ SSL/Avstånd mellan lins hjärnceller är för stort.py
@@ -1,6 +1,6 @@
 import math
 import time
-import os
+
 def distance(A,B):
     dx = A[0]-B[0]
     dy = A[1]-B[1]
@@ -11,23 +11,24 @@ def distance(A,B):
 A = []
 B = []
 play = True
+
 while play:
-    Apunkt1 = int(input("mata in x punkten i A : "))
+    Apunkt1 = int(input("mata in X punkten i A : "))
     A.append(Apunkt1)
-    Apunkt2 = int(input("\nmata in y punkten i A : "))
+    Apunkt2 = int(input("\nmata in Y punkten i A : "))
     A.append(Apunkt2)
-    Apunkt3 = int(input("\nmata in z punkten i A : "))
+    Apunkt3 = int(input("\nmata in Z punkten i A : "))
     A.append(Apunkt3)
-    Bpunkt1 = int(input("\n\nmata in x punkten i B : "))
+    Bpunkt1 = int(input("\n\nmata in X punkten i B : "))
     B.append(Bpunkt1)
-    Bpunkt2 = int(input("m\nata in y punkten i B : "))
+    Bpunkt2 = int(input("m\nata in Y punkten i B : "))
     B.append(Bpunkt2)
-    Bpunkt3 = int(input("\nmata in z punkten i B : "))
+    Bpunkt3 = int(input("\nmata in Z punkten i B : "))
     B.append(Bpunkt3)
 
     svar = distance(A,B)
     print (f"\nAnvståndet mellan {A} och {B} är : {svar:.2f}")
-    time.sleep(1.5)
+    time.sleep(1)
     A.clear()
     B.clear()
     again = input("Vill du stoppa? y/n : ").lower()
